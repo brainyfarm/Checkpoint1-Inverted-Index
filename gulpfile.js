c403239e0476f272c8ab11804b7a5c1c3087aa79
Olawale Akinseye <andeladev@Sayos-MacBook-Pro.local>
2016-11-11 07:20:18 +0100

    Updated gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict'
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    })
+})
