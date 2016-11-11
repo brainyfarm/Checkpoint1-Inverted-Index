88b18bb996d4c1bedb1e9ae555ecfdc41782d0bb
Olawale Akinseye <andeladev@Sayos-MacBook-Pro.local>
2016-11-11 07:34:55 +0100

    Setup of gulp watching

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
     browserSync.init({
         server: {
             baseDir: 'src'
         }
     });
+});
+
+gulp.task('watch', ['browserSync'], function () {
+    // Reload browser when files change
+    gulp.watch('src/css/**/*.css', browserSync.reload);
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('jasmine/spec/**/*.js', browserSync.reload);
 });
