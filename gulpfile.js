ca52db3831a6deae001c454771fce6b019cf39e5
Olawale Akinseye <brainyfarm@gmail.com>
2016-11-14 21:07:38 +0100

    Added gulp runner file

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict'
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+});
+
+gulp.task('start', ['browserSync'], function () {
+    // Reload browser when files change
+    gulp.watch('src/css/**/*.css', browserSync.reload);
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('jasmine/spec/**/*.js', browserSync.reload);
+});
