5dec7a3f56ab4cb379103af2ac6c35f6d6d4d01c
Olawale Akinseye <brainyfarm@gmail.com>
2016-12-03 16:46:55 +0100

    add dummy data

---
+++ jasmine/books-data.js
@@ -0,0 +1,16 @@
+const book = [
+  {
+    "title": "Alice in Wonderland",
+    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  },
+
+  {
+    "title": "The Lord of the Rings: The Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+  {
+    "title": "Animal Farm",
+    "text": "All animals are equal but some animals are equal than others - George Orwell"
+  }
+]
+
